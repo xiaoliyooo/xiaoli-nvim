3573a1a9fe99de36d55af3a7ab7f8b178df8a467
xiaoliyo <2585960098@qq.com>
2025-08-30 16:45:06 +0800

    chore: delete rename plugin

--- lua/plugins/lsp/lspsaga.lua
+++ lua/plugins/lsp/lspsaga.lua
@@ -25,7 +25,7 @@ return function()
   vim.api.nvim_create_autocmd('VimEnter', {
     callback = function()
       -- vim.keymap.set('n', 'gh', '<cmd>Lspsaga hover_doc<CR>')
-      -- vim.keymap.set('n', '<leader>rn', '<cmd>Lspsaga rename<CR>')
+      vim.keymap.set('n', '<leader>rn', '<cmd>Lspsaga rename<CR>')
       vim.keymap.set('n', '<leader>ca', '<cmd>Lspsaga code_action<CR>')
       vim.keymap.set('n', '<leader>le', '<cmd>Lspsaga show_buf_diagnostics<CR>')
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
