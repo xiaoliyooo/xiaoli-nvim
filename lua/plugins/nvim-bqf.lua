ca38d2b80fcbe930c1813b39bef82182cce20f5f
xiaoli <2585960098@qq.com>
2025-09-02 13:36:34 +0800

    chore: bqf config

--- lua/plugins/nvim-bqf.lua
+++ lua/plugins/nvim-bqf.lua
@@ -4,18 +4,17 @@ return {
   'kevinhwang91/nvim-bqf',
   ft = 'qf',
   config = function()
+    local border = require('core.custom-style').border
     require('bqf').setup({
       auto_enable = true,
       auto_resize_height = true, -- highly recommended enable
       preview = {
-        win_height = 24,
-        win_vheight = 12,
+        win_height = 18,
+        win_vheight = 18,
         delay_syntax = 80,
-        border = { '┏', '━', '┓', '┃', '┛', '━', '┗', '┃' },
+        border = border,
         show_title = true,
-        should_preview_cb = function(bufnr, qwinid)
-          return true
-        end,
+        auto_preview = true,
       },
       -- make `drop` and `tab drop` to become preferred
       func_map = {
