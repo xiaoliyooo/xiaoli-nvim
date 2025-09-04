08853a01d73bea96eae0f84da9e29261f6fb0941
xiaoliyo <2585960098@qq.com>
2025-09-04 21:52:50 +0800

    chore: telescope fzf config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,6 +11,7 @@ return {
     'nvim-telescope/telescope-ui-select.nvim',
     'nvim-telescope/telescope-live-grep-args.nvim',
     'nvim-telescope/telescope-frecency.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
   },
   build = 'brew install ripgrep',
   event = 'VimEnter',
@@ -91,9 +92,16 @@ return {
             },
           },
         },
+        fzf = {
+          fuzzy = true, -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
+        },
       },
     })
 
+    telescope.load_extension('fzf')
     telescope.load_extension('live_grep_args')
     telescope.load_extension('ui-select')
     telescope.load_extension('frecency')
