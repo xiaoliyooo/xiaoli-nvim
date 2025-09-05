826a3e172ed4a30f595acea273c30499cb0a53c5
xiaoli <2585960098@qq.com>
2025-09-05 16:36:42 +0800

    chore: comment keymaps

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -3,7 +3,17 @@
 return {
   'numToStr/Comment.nvim',
   enabled = true,
-  opts = {
-    -- add any options here
-  },
+  config = function()
+    require('Comment').setup({
+      ---LHS of extra mappings
+      extra = {
+        ---Add comment on the line above
+        above = 'gcO',
+        ---Add comment on the line below
+        below = 'gco',
+        ---Add comment at the end of line
+        eol = 'gca',
+      },
+    })
+  end,
 }
