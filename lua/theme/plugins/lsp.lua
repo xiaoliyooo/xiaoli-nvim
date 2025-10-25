031469951acb7e6040cb0ee91f2d896e81b03197
xiaoliyo <2585960098@qq.com>
2025-10-25 11:46:53 +0800

    chore: inlayhint fg

--- lua/theme/plugins/lsp.lua
+++ lua/theme/plugins/lsp.lua
@@ -3,7 +3,7 @@ local color_table = require('core.custom-style').color_table
 local M = {}
 
 function M.reset()
-  vim.api.nvim_set_hl(0, 'LspInlayHint', { fg = color_table.light_green })
+  vim.api.nvim_set_hl(0, 'LspInlayHint', { fg = '#8d8d8e' })
 end
 
 return M
