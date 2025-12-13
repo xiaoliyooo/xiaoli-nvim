bb2f80fbd754e04b061e774c1cd8702493e2aa1e
xiaoli <2585960098@qq.com>
2025-12-13 10:35:11 +0800

    feat: multicursor.nvim

--- lua/plugins/multi-cursor.lua
+++ lua/plugins/multi-cursor.lua
@@ -1,12 +1,70 @@
 -- multi cursor
 
+-- return {
+--   'mg979/vim-visual-multi',
+--   event = 'VeryLazy',
+--   enabled = false,
+--   init = function()
+--     vim.g.VM_maps = {
+--       ['Add Cursor Down'] = '<C-]>',
+--       ['Add Cursor Up'] = '<C-[>',
+--     }
+--   end,
+-- }
+
 return {
-  'mg979/vim-visual-multi',
-  event = 'VeryLazy',
-  init = function()
-    vim.g.VM_maps = {
-      ['Add Cursor Down'] = '<C-]>',
-      ['Add Cursor Up'] = '<C-[>',
-    }
+  'jake-stewart/multicursor.nvim',
+  branch = '1.0',
+  config = function()
+    local mc = require('multicursor-nvim')
+    mc.setup()
+    local set = vim.keymap.set
+    set({ 'n', 'x' }, '<up>', function()
+      mc.lineAddCursor(-1)
+    end)
+    set({ 'n', 'x' }, '<down>', function()
+      mc.lineAddCursor(1)
+    end)
+    set({ 'n', 'v' }, '<C-n>', function()
+      mc.matchAddCursor(1)
+    end)
+    set({ 'n', 'x' }, '<C-s>', function()
+      mc.matchSkipCursor(1)
+    end)
+
+    -- set({ 'n', 'v' }, '<C-p>', function()
+    --   mc.matchAddCursor(-1)
+    -- end)
+    -- set({"n", "x"}, "<leader>S", function() mc.matchSkipCursor(-1) end)
+
+    set('x', 'I', mc.insertVisual)
+    set('x', 'A', mc.appendVisual)
+    mc.addKeymapLayer(function(layerSet)
+      -- Select a different cursor as the main one.
+      layerSet({ 'n', 'x' }, '<left>', mc.prevCursor)
+      layerSet({ 'n', 'x' }, '<right>', mc.nextCursor)
+
+      -- Delete the main cursor.
+      layerSet({ 'n', 'x' }, '<leader>x', mc.deleteCursor)
+
+      -- Enable and clear cursors using escape.
+      layerSet('n', '<esc>', function()
+        if not mc.cursorsEnabled() then
+          mc.enableCursors()
+        else
+          mc.clearCursors()
+        end
+      end)
+    end)
+    -- Customize how cursors look.
+    local hl = vim.api.nvim_set_hl
+    hl(0, 'MultiCursorCursor', { reverse = true })
+    -- hl(0, 'MultiCursorVisual', { link = 'Visual' })
+    hl(0, 'MultiCursorVisual', { bg = '#b294bb', fg = '#ffffff', bold = true })
+    hl(0, 'MultiCursorSign', { link = 'SignColumn' })
+    hl(0, 'MultiCursorMatchPreview', { link = 'Search' })
+    hl(0, 'MultiCursorDisabledCursor', { reverse = true })
+    hl(0, 'MultiCursorDisabledVisual', { link = 'Visual' })
+    hl(0, 'MultiCursorDisabledSign', { link = 'SignColumn' })
   end,
 }
