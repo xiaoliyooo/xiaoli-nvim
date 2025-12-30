60c2a0e1b08f5cc7e612aaeaaff225f0e4a63ae9
xiaoli <2585960098@qq.com>
2025-12-30 14:01:58 +0800

    chore: light_green 环境变量

--- lua/core/custom-style.lua
+++ lua/core/custom-style.lua
@@ -1,6 +1,6 @@
 local style = {
   color_table = {
-    light_green = '#0bf432',
+    light_green = os.getenv('LIGHT_GREEN'),
     error_color = '#ff6b6b',
     warn_color = '#ffff44',
     info_color = '#44ffff',
