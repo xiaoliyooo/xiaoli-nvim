180380a8d76701bc910ce3c794ffe4f15bb5c57c
xiaoli <2585960098@qq.com>
2026-01-04 09:31:33 +0800

    chore: emmet config

--- lua/plugins/lsp/servers/emmet.lua
+++ lua/plugins/lsp/servers/emmet.lua
@@ -2,14 +2,11 @@ return function()
   vim.lsp.config('emmet_language_server', {
     filetypes = {
       'css',
-      'eruby',
       'html',
-      'javascript',
-      'javascriptreact',
       'less',
       'sass',
       'scss',
-      'pug',
+      'javascriptreact',
       'typescriptreact',
     },
     -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
