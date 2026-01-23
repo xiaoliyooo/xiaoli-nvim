4ff1d54287e605d020862adcfe5c934a6f7050a8
xiaoli <2585960098@qq.com>
2026-01-23 15:48:05 +0800

    chore: mini indent

--- lua/plugins/gruber-darker.lua
+++ lua/plugins/gruber-darker.lua
@@ -90,7 +90,7 @@ return {
         vim.api.nvim_set_hl(0, 'TelescopeSelectionCaret', { fg = theme_color, bg = '#3a3a3a' })
         vim.api.nvim_set_hl(0, 'TelescopeMatching', { fg = theme_color, bold = true })
 
-        vim.g.miniindentscope_disable = true
+        -- vim.g.miniindentscope_disable = true
       end,
     })
 
