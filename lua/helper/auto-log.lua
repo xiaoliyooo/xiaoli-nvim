ba885b8a7898a99ef3aef5c3ebb41b6060e9e499
xiaoli <2585960098@qq.com>
2026-02-02 13:50:56 +0800

    feat: log支持lua和rust

--- lua/helper/auto-log.lua
+++ lua/helper/auto-log.lua
@@ -1,11 +1,22 @@
 local M = {}
 
+local log_formats = {
+  javascript = 'console.log(\'<C-R>a:\', <C-R>a);',
+  lua = 'print(\'<C-R>a:\', vim.inspect(<C-R>a))',
+  rust = 'println!("<C-R>a: {:?}", <C-R>a);',
+}
+
+local default_format = log_formats.javascript
+
 function M.auto_console_log()
+  local ft = vim.bo.filetype
+  local template = log_formats[ft] or default_format
   local mode = vim.api.nvim_get_mode().mode
-  if mode:match('^[vV\22]') then
-    return '"ayoconsole.log(\'<C-R>a:\', <C-R>a);<Esc>'
+
+  if mode:match('^[vV\\22]') then
+    return '"ayo' .. template .. '<Esc>'
   end
-  return '"ayiwoconsole.log(\'<C-R>a:\', <C-R>a);<Esc>'
+  return '"ayiwo' .. template .. '<Esc>'
 end
 
 return M
