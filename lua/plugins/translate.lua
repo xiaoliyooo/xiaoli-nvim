310e536a85f6f880d20b03bbcdc6d244b3d29a8d
xiaoli <2585960098@qq.com>
2026-02-03 20:12:34 +0800

    feat: 优化可视化翻译的范围

--- lua/plugins/translate.lua
+++ lua/plugins/translate.lua
@@ -1,24 +1,21 @@
 -- translation
+-- 使用 pantran.nvim 内置 API 实现翻译
 
 return {
   'potamides/pantran.nvim',
   cmd = { 'Pantran', 'Translate', 'TranslateCN', 'TranslateEN', 'TranslateKO' },
   keys = {
-    { '<leader>tr', mode = { 'n', 'v' }, desc = '翻译文本' },
+    { '<leader>tc', mode = { 'n', 'x' }, desc = '翻译为中文' },
+    { '<leader>te', mode = { 'n', 'x' }, desc = '翻译为英文' },
+    -- { '<leader>tr', mode = { 'n', 'x' }, desc = '翻译为韩文' },
   },
   config = function()
     local pantran = require('pantran')
+
     pantran.setup({
-      -- Default engine to use for translation. To list valid engine names run
-      -- `:lua =vim.tbl_keys(require("pantran.engines"))`.
       default_engine = 'google',
-      -- Configuration for individual engines goes here.
       engines = {
         google = {
-          -- Default languages can be defined on a per engine basis. In this case
-          -- `:lua require("pantran.async").run(function()
-          -- vim.pretty_print(require("pantran.engines").yandex:languages()) end)`
-          -- can be used to list available language identifiers.
           default_source = 'auto',
           default_target = 'en',
         },
@@ -27,57 +24,42 @@ return {
         mappings = {
           edit = {
             n = {
-              -- Use this table to add additional mappings for the normal mode in
-              -- the translation window. Either strings or function references are
-              -- supported.
               ['j'] = 'gj',
               ['k'] = 'gk',
             },
             i = {
-              -- Similar table but for insert mode. Using 'false' disables
-              -- existing keybindings.
               ['<C-y>'] = false,
               ['<C-a>'] = require('pantran.ui.actions').yank_close_translation,
             },
           },
-          -- Keybindings here are used in the selection window.
           select = {
             n = {},
           },
         },
       },
     })
-    vim.api.nvim_create_user_command('Translate', function(opts)
-      vim.cmd('Pantran')
-    end, {
-      desc = '翻译文本（Pantran 的别名）',
-    })
 
-    vim.keymap.set('n', '<leader>tr', '<CMD>Pantran<CR>', { desc = '翻译文本窗口' })
+    local opts = { noremap = true, silent = true, expr = true }
 
-    vim.keymap.set(
-      'v',
-      '<leader>tr',
-      ':\'<,\'>Pantran target=zh-CN<CR>',
-      { desc = '翻译选中文本', silent = true, noremap = true }
-    )
+    vim.keymap.set('n', '<leader>tc', function()
+      return pantran.motion_translate({ target = 'zh-CN' }) .. 'iw'
+    end, opts)
+    vim.keymap.set('x', '<leader>tc', function()
+      return pantran.motion_translate({ target = 'zh-CN' })
+    end, opts)
 
-    vim.api.nvim_create_user_command('TranslateCN', function()
-      require('pantran').range_translate({
-        target = 'zh-CN',
-      })
-    end, { desc = '翻译为中文' })
+    vim.keymap.set('n', '<leader>te', function()
+      return pantran.motion_translate({ target = 'en' }) .. 'iw'
+    end, opts)
+    vim.keymap.set('x', '<leader>te', function()
+      return pantran.motion_translate({ target = 'en' })
+    end, opts)
 
-    vim.api.nvim_create_user_command('TranslateEN', function()
-      require('pantran').range_translate({
-        target = 'en',
-      })
-    end, { desc = '翻译为英语' })
-
-    vim.api.nvim_create_user_command('TranslateKO', function()
-      require('pantran').range_translate({
-        target = 'ko',
-      })
-    end, { desc = '翻译为韩语' })
+    -- vim.keymap.set('n', '<leader>tr', function()
+    --   return pantran.motion_translate({ target = 'ko' }) .. 'iw'
+    -- end, opts)
+    -- vim.keymap.set('x', '<leader>tr', function()
+    --   return pantran.motion_translate({ target = 'ko' })
+    -- end, opts)
   end,
 }
