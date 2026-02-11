2f514d7cf88416e41c9a7059207b76948c6fde4e
xiaoli <2585960098@qq.com>
2026-02-11 17:41:53 +0800

    feat: 交换scrollback y快捷键

--- lua/plugins-minimal/kitty-scrollback.lua
+++ lua/plugins-minimal/kitty-scrollback.lua
@@ -1,7 +1,16 @@
 return {
   'mikesmithgh/kitty-scrollback.nvim',
-  event = 'BufReadPre',
+  event = { 'User KittyScrollbackLaunch' },
   build = [[nvim --headless +'KittyScrollbackGenerateKittens']],
+  init = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'kitty-scrollback',
+      callback = function()
+        vim.keymap.set('v', 'y', '"+y', { buffer = true })
+        vim.keymap.set('v', '<leader>y', 'y', { buffer = true })
+      end,
+    })
+  end,
   config = function()
     require('kitty-scrollback').setup({
       status_window = {
