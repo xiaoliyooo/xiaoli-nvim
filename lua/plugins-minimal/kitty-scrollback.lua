32230472fef3a4ab7685fffca5b8b29425520772
xiaoli <2585960098@qq.com>
2026-02-12 10:17:46 +0800

    chore: scrollback快捷键

--- lua/plugins-minimal/kitty-scrollback.lua
+++ lua/plugins-minimal/kitty-scrollback.lua
@@ -2,15 +2,15 @@ return {
   'mikesmithgh/kitty-scrollback.nvim',
   event = { 'User KittyScrollbackLaunch' },
   build = [[nvim --headless +'KittyScrollbackGenerateKittens']],
-  init = function()
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = 'kitty-scrollback',
-      callback = function()
-        vim.keymap.set('v', 'y', '"+y', { buffer = true })
-        vim.keymap.set('v', '<leader>y', 'y', { buffer = true })
-      end,
-    })
-  end,
+  -- init = function()
+  --   vim.api.nvim_create_autocmd('FileType', {
+  --     pattern = 'kitty-scrollback',
+  --     callback = function()
+  --       vim.keymap.set('v', 'y', '"+y', { buffer = true })
+  --       vim.keymap.set('v', '<leader>y', 'y', { buffer = true })
+  --     end,
+  --   })
+  -- end,
   config = function()
     require('kitty-scrollback').setup({
       status_window = {
