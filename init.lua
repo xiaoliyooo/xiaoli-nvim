55dba0cb7db4b03007bfdc320e881c60ad83fb07
xiaoli <2585960098@qq.com>
2026-02-13 13:50:55 +0800

    feat: nvimpager配置

--- init.lua
+++ init.lua
@@ -5,3 +5,9 @@ if require('helper.env').is_kitty_scrollback() then
   vim.cmd('colorscheme industry')
   vim.api.nvim_set_hl(0, 'FlashLabel', { bg = 'Red', fg = 'White', bold = true })
 end
+
+-- nvimpager 打开
+local status, nvimpager = pcall(require, 'nvimpager')
+if status then
+  nvimpager.maps = false
+end
