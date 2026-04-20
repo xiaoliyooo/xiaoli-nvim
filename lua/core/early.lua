edf4cc9c1c51092bcf37f5c9afc0327aba580053
xiaoli <2585960098@qq.com>
2026-04-20 14:50:13 +0800

    chore: man插件打开

--- lua/core/early.lua
+++ lua/core/early.lua
@@ -14,7 +14,6 @@ vim.g.loaded_vimball = 1
 vim.g.loaded_vimballPlugin = 1
 vim.g.loaded_2html_plugin = 1
 vim.g.loaded_tutor_mode_plugin = 1
-vim.g.loaded_man = 1
 vim.g.loaded_remote_plugins = 1
 vim.g.loaded_shada_plugin = 1
 vim.g.loaded_spellfile_plugin = 1
